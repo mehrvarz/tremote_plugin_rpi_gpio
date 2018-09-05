1df028761a6ef83045e489f7dd61c1e766cf7ba2
timur.mehrvarz <timur.mehrvarz@riseup.net>
2018-09-05 20:07:02 +0200

    added line breaks

--- rpi_gpio.go
+++ rpi_gpio.go
@@ -20,13 +20,15 @@ const (
 )
 
 var (
-	log1                log.Logger
+	logm                log.Logger
 	instanceNumber      = 0
 	rpioOpen            = false
 )
 
-func Action(log log.Logger, pid int, longpress bool, pressedDuration int64, rcs* tremote_plugin.RemoteControlSpec, ph tremote_plugin.PluginHelper, wg *sync.WaitGroup) error {
-	log1 = log
+func Action(log log.Logger, pid int, longpress bool, pressedDuration int64, 
+			rcs* tremote_plugin.RemoteControlSpec, ph tremote_plugin.PluginHelper, 
+			wg *sync.WaitGroup) error {
+	logm = log
 
 	if instanceNumber==0 {
 		firstinstance()
@@ -40,7 +42,7 @@ func Action(log log.Logger, pid int, longpress bool, pressedDuration int64, rcs*
 
 	pinnumber, err := strconv.Atoi(strArray[0])
 	if err != nil {
-		log1.Warningf("%s arg[0]=%s to int failed err=",pluginname,strArray[0],err.Error())
+		logm.Warningf("%s arg[0]=%s to int failed err=",pluginname,strArray[0],err.Error())
 		return err
     }
 
@@ -48,22 +50,22 @@ func Action(log log.Logger, pid int, longpress bool, pressedDuration int64, rcs*
 	
 	if !rpioOpen {
 		if err := rpio.Open(); err != nil {
-			log1.Warningf("%s rpio.Open() failed err=%s",pluginname,err.Error())
+			logm.Warningf("%s rpio.Open() failed err=%s",pluginname,err.Error())
 			return err
 		}
 		//Because this plugin will stay in memory, we leave rpio open for upcoming tasks
 		//defer rpio.Close()
 		rpioOpen = true
-       	log1.Infof("%s rpio opened; use pin %d for output",pluginname,pinnumber)
+       	logm.Infof("%s rpio opened; use pin %d for output",pluginname,pinnumber)
 		pin.Output()
 	}
 
 	if pressedDuration==0 {
-		log1.Infof("%s button pressed: pin %d high",pluginname,pinnumber)
+		logm.Infof("%s button pressed: pin %d high",pluginname,pinnumber)
 		ph.PrintInfo(fmt.Sprintf("pin %d high",pinnumber))
 		pin.High()
 	} else {
-		log1.Infof("%s button released: pin %d low",pluginname,pinnumber)
+		logm.Infof("%s button released: pin %d low",pluginname,pinnumber)
 		ph.PrintInfo(fmt.Sprintf("pin %d low",pinnumber))
 		pin.Low()
 	}
